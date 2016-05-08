fc51d1c8724dc50ff6800aeb35d81b3b38b35781
malloc84 <obi.echo@gmail.com>
2016-05-08 11:05:58 +0200

    HelloFishingPlanet
    HelloFishingPlanet Application

---
+++ HelloFishingPlanet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloFishingPlanet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple HelloWorld Application
+            // Continue the code...
+            Console.WriteLine("Welcome to Fishing Planet Tools and Source Code Repository");
+
+            // Press any key to close the window
+            Console.ReadKey();
+        }
+    }
+}
